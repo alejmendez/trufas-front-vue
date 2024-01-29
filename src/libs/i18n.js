8509e21d5bdc99cf4e8ea4580725c5131828938e
Alejandro Méndez <alejmendez.87@gmail.com>
2024-01-28 22:53:38 -0300

    add languages

--- src/libs/i18n.js
+++ src/libs/i18n.js
@@ -1,9 +1,14 @@
 import { createI18n } from 'vue-i18n'
+import es from '@/lang/es.json'
 
 export const initI18n = (app) => {
   app.use(
     createI18n({
-      // something vue-i18n options here ...
+      legacy: false,
+      locale: 'es',
+      messages: {
+        es,
+      }
     })
   )
 }
