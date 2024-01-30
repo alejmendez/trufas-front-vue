ef3d7a8d5833eb8fe3a7fc0c72ff4c2ba1f3fae3
Alejandro Méndez <alejmendez.87@gmail.com>
2024-01-30 00:12:01 -0300

    add axios interceptor error 401

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -4,4 +4,18 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
 
+instance.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    if (401 === error.response.status) {
+      localStorage.clear();
+      window.location.href = "/login";
+    } else {
+      return Promise.reject(error)
+    }
+  }
+)
+
 export default instance
